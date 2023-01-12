b76302628b7875418c17ca28c8002b7fe8a936b2
UK <gang3351@gmail.com>
2023-01-12 17:20:52 +0900

    2023/01/12 스위프트 문법 공부

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -386,6 +386,30 @@ func connectUser(to server: String) async {
     print(greeting)
 }
 Task {
-    await connectUser(to: "primary")
+    //await connectUser(to: "primary")
 }
-print("asd")
+
+protocol ExampleProtocol {
+    var simpleDescription: String {get}
+    mutating func adjust()
+}
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += " Now 100% adjusted."
+    }
+}
+var a = SimpleClass()
+a.adjust()
+let aDesrciption = a.simpleDescription
+struct SimpleStructure: ExampleProtocol {
+    var simpleDescription: String = "A simple structure."
+    mutating func adjust() {
+        simpleDescription += " (adjusted)"
+    }
+}
+var b = SimpleStructure()
+b.adjust()
+let bDescription = b.simpleDescription
+print(aDesrciption, bDescription)
