51c2549bf7a910afa1b15375d74ba5be53a51858
UK <gang3351@gmail.com>
2023-01-16 15:22:57 +0900

    스위프트 문법 공부 2023/01/16

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -412,4 +412,98 @@ struct SimpleStructure: ExampleProtocol {
 var b = SimpleStructure()
 b.adjust()
 let bDescription = b.simpleDescription
-print(aDesrciption, bDescription)
+//print(aDesrciption, bDescription)
+
+extension Int: ExampleProtocol {
+    var simpleDescription: String {
+        return "The number \(self)"
+    }
+    mutating func adjust() {
+        self += 42
+    }
+}
+/*
+print(7.simpleDescription)
+var aaa = 7
+aaa.adjust()
+print(aaa)
+*/
+
+enum PrinterError : Error {
+    case outOfPaper
+    case noToner
+    case onFire
+}
+func send(job: Int, toPrinter printerName: String) throws -> String {
+    if printerName == "Never has Toner" {
+        throw PrinterError.noToner
+    }
+    return "job sent"
+}
+/*
+do {
+    let printerResponse = try send(job: 1040, toPrinter: "Never has Toner")
+    print(printerResponse)
+} catch {
+    print(error)
+}
+
+do {
+    let printerResponse = try send(job: 1440, toPrinter: "Gutenberg")
+    print(printerResponse)
+} catch PrinterError.onFire {
+    print("I'll just put this over here, with the rest of the fire.")
+} catch let printerError as PrinterError {
+    print("Printer error: \(printerError)")
+} catch {
+    print(error)
+}
+*/
+
+let printerSucces = try? send(job: 1884, toPrinter: "Mergenthaler")
+let printerFailure = try? send(job: 1885, toPrinter: "Never has Toner")
+//print("\(printerSucces), \(printerFailure)")
+
+var fridgeIsOpen = false
+let fridgeContent = ["milk", "eggs", "leftovers"]
+func fridgeContains(_ food: String) -> Bool {
+    fridgeIsOpen = true
+    defer {
+        fridgeIsOpen = false
+    }
+    let result = fridgeContent.contains(food)
+    return result
+}
+fridgeContains("eggs")
+//print(fridgeIsOpen)
+
+func makeArray<Item>(repeating item: Item, numberOfTimes: Int) -> [Item] {
+    var result: [Item] = []
+    for _ in 0..<numberOfTimes {
+        result.append(item)
+    }
+    return result
+}
+//print(makeArray(repeating: "knock", numberOfTimes: 4))
+
+enum OptionalValue<Wrapped> {
+    case none
+    case some(Wrapped)
+}
+var possibleInteger: OptionalValue<Int> = .none
+possibleInteger = .some(100)
+
+
+func anyCommonElements<T: Sequence, U: Sequence>(_ lhs: T, _ rhs: U)->Bool
+where T.Element: Equatable, T.Element == U.Element
+{
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                return true
+            }
+        }
+    }
+    return false
+}
+print(anyCommonElements([1,2,3], [3]))
